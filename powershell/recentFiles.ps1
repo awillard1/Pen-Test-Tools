62e67bd94d858228c12dce2fd313a9453dada088
Adam Willard <awillard@gmail.com>
2017-05-25 10:29:37 -0400

    update

---
+++ powershell/recentFiles.ps1
@@ -0,0 +1,20 @@
+﻿function GetRecentlyUsedFiles(){
+    foreach($user in (Get-ChildItem "$env:SystemDrive\Users")) {
+        $recentpath = "C:\Users\$user\AppData\Roaming\Microsoft\Windows\Recent\"
+        if (Test-Path($recentpath))
+        {
+            $recentdocs = Get-childItem $recentpath
+            foreach($doc in $recentdocs){
+                try{
+                    $filepath = $recentpath + $doc
+                    $sh = New-Object -ComObject WScript.Shell
+                    $target = $sh.CreateShortcut($filepath).TargetPath
+                    if (![string]::IsNullOrEmpty($target)){
+                        $target
+                        }
+                    }
+                catch {}
+            }
+        }
+    }
+}
