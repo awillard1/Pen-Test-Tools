0e6161902b9a9f49ab58ba4d89b13864a17ff1fb
Adam Willard <awillard@gmail.com>
2017-06-23 11:23:50 -0400

    Payload exe project
    This project is a simple .net application to allow the logging of a fake
    malicious file uploaded and then executed.

---
+++ exe-dot-net/payload/payload/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace payload
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var data = System.Diagnostics.Process.GetCurrentProcess().ProcessName;
+            System.Diagnostics.Process.Start("https://www.aswsec.com/pen-test/exe-logged.html?" + data);
+        }
+    }
+}
