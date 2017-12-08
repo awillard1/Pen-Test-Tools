fc595d6c440d58e2e339dad5a5b5487b8b1161d8
Adam Willard <awillard@foregroundsecurity.com>
2017-12-07 20:39:31 -0500

    Create rf.php

---
+++ xss/request-forgery/rf.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+</head>
+<body>
+<p>FAKE TOKEN<input type="text" name="token" value="welldoesitwork"></p>
+</body>
+</html>
