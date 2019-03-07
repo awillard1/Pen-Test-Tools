2669d524e72e9b98a12427c164666be2d1906b19
Adam Willard <awillard@gmail.com>
2019-03-07 13:13:54 -0500

    Create poc-apos.php

---
+++ escapes/php-poc/poc-apos.php
@@ -0,0 +1,8 @@
+<?php
+$fp = @fopen('test.txt','r');
+$hack = $_GET['s'];
+header("Content-Disposition: attachment; filename='test.html".$hack.".txt'");
+header("Content-Length:".filesize($fp));
+fpassthru($fp);
+fclose($fp)
+?>
