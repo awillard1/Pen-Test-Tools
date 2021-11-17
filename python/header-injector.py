3cd54171c9f023d6097666571d9e5420eca8f74a
Adam Willard <awillard@gmail.com>
2021-11-17 10:35:58 -0500

    just making changes

--- python/header-injector.py
+++ python/header-injector.py
@@ -12,6 +12,30 @@ _proxy = 'http://127.0.0.1:8080'
 dir = os.path.dirname(os.path.realpath(__file__))
 headerPayload = "%0D%0A`~!@#$%^&*()_+{}=-[]:;'\"\\?/><.,AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA"
 
+class RespResult:
+    """Simple Object"""
+    def __init__(self, status_code="", contentLength="", isLenDifferent=False, isPayloadInBody=False):
+        self.status_code = status_code
+        self.contentLength = contentLength
+        self.isLenDifferent = isLenDifferent
+        self.isPayloadInBody = isPayloadInBody
+        
+def process_response(resp, baselineLen):
+    retval = RespResult()
+    if baselineLen != len(resp.content):
+        retval.isLenDifferent = True
+    else:
+        retval.isLenDifferent = False
+    
+    if headerPayload in str(resp.content):
+        retval.isPayloadInBody = True
+    else:
+        retval.isPayloadInBody = False
+        
+    retval.status_code = str(resp.status_code)
+    retval.contentLength = str(len(resp.content))
+    return retval 
+  
 def get_random_useragent():
     """Returns a randomly chosen User-Agent string."""
     win_edge = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
@@ -43,20 +67,27 @@ def injectHeaders(url, cookies):
     print("\nPayload to be passed:\n" + headerPayload)
     ua = get_random_useragent()
     print("\nUsing the User-Agent:  " +ua)
+
     with requests.Session() as s:
         s.proxies['http'] = _proxy
         s.proxies['https'] = _proxy
         
         print("\nSend Initial Request - TODO use for comparison\n")
-        r1 = requests.get(url, headers={'User-Agent': ua},cookies=cookies, verify=False)
+        resp = requests.get(url, headers={'User-Agent': ua},cookies=cookies, verify=False)
+        baselineLen = len(resp.content)
+        print("Baseline Request - Status: " + str(baselineLen) + " - Content-Length: " + str(len(resp.content)) + "\n")
         print("\nSend 1 header in each request.\n")
         for h in _headers:
             hdrs = {'User-Agent':ua, h : headerPayload }
-            r1 = requests.get(url, cookies=cookies,headers=hdrs, verify=False)
-        
+            resp = requests.get(url, cookies=cookies,headers=hdrs, verify=False)
+            result = process_response(resp, baselineLen)
+            difstr=""
+            if result.isLenDifferent:
+                difstr = "*"
+            print(h + ": Status: " + result.status_code + " - Content-Length: " + ("","*")[result.isLenDifferent] + result.contentLength + " - Contains Payload: " + str(result.isPayloadInBody) +"\r")
         print("\nSend 1 massive request\n")
         allheaders = {}
-        allheaders["hack"]="me"
+
         for h in _headers: 
             s = h.lower()
             if s == "host":
@@ -66,8 +97,11 @@ def injectHeaders(url, cookies):
             else:
                 allheaders[h]=headerPayload
         
-        r1 = requests.get(url, cookies=cookies, headers=allheaders, verify=False)
-    
+        resp = requests.get(url, cookies=cookies, headers=allheaders, verify=False)
+        result = process_response(resp, baselineLen)
+        print("All Header Request - Status: " + result.status_code + " - Content-Length: " + result.contentLength + " - Contains Payload: " + str(result.isPayloadInBody) +"\n")
+  
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument("-pr", "--proxy", 
