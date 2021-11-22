c60e9f22b28c61e1fe5bcba503603e0fc2b3d4e9
Adam Willard <awillard@gmail.com>
2021-11-22 17:34:40 -0500

    Create appapitoken.py

---
+++ python/appapitoken.py
@@ -0,0 +1,26 @@
+import requests
+import json
+import datetime
+
+class tokenGenerate:
+    def __init__(self, url="", payload=""):
+        self.url = url
+        self.payload = payload
+        self.headers = {"Content-Type": "application/x-www-form-urlencoded"}
+        self.time_expired = datetime.datetime.now()
+        self.expired = False
+        self.token = {}
+        self.json_data = {}
+
+    def getToken(self):
+        if self.time_expired <= datetime.datetime.now():
+            response = requests.request("POST", self.url, headers=self.headers, data=self.payload)
+            data = response.text
+            self.json_data = json.loads(data)
+            expiresin = self.json_data["expires_in"]
+            self.token = data
+            self.time_expired = datetime.datetime.now() + datetime.timedelta(0,(int(expiresin)-100)) 
+
+        return {"Authorization": self.json_data["token_type"] + " " + self.json_data["access_token"]}
+       
+
