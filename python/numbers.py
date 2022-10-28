a4ec6ed247bd720bbb411fb08d6f119bd86fd863
Adam Willard <awillard@gmail.com>
2022-10-28 12:09:48 -0400

    Add files via upload

--- python/numbers.py
+++ python/numbers.py
@@ -1,7 +1,6 @@
 # based on https://www.codesansar.com/python-programming-examples/number-words-conversion-no-library-used.htm
-# useful to run with the wordtorule ptyhon script to convert to a rule file
-# Number to Words
-# Main Logic
+import argparse
+
 ones = ('Zero', 'One', 'Two', 'Three', 'Four', 'Five', 'Six', 'Seven', 'Eight', 'Nine')
 twos = ('Ten', 'Eleven', 'Twelve', 'Thirteen', 'Fourteen', 'Fifteen', 'Sixteen', 'Seventeen', 'Eighteen', 'Nineteen')
 tens = ('Twenty', 'Thirty', 'Forty', 'Fifty', 'Sixty', 'Seventy', 'Eighty', 'Ninety', 'Hundred')
@@ -10,47 +9,27 @@ suffixes = ('', 'Thousand', 'Million', 'Billion')
 def process(number, index, ln):
     if number=='0':
         return 'Zero'
-    
     length = len(number)
     if(length > 3):
         return False
-    
     number = number.zfill(3)
     words = ''
     hdigit = int(number[0])
     tdigit = int(number[1])
     odigit = int(number[2])
     words += '' if number[0] == '0' else ones[hdigit]
-    words += ' Hundred ' if not words == '' else ''
-    
-    if index==0 and ln>3:
-        words+=' and '
-    elif words=='':
-        words+=''
-    elif index==0 and tdigit==0 and odigit==0:
-        words+=''
-    elif index==0:
-        words+= ' and '
-    else:
-        words+=''
-    
+    words += 'Hundred' if not words == '' else ''
     if(tdigit > 1):
         words += tens[tdigit - 2]
-        words += ''
         words += ones[odigit]
-    
     elif(tdigit == 1):
         words += twos[(int(tdigit + odigit) % 10) - 1]
     elif(tdigit == 0):
         words += ones[odigit]
     if(words.endswith('Zero')):
         words = words[:-len('Zero')]
-    else:
-        words += ''
-     
     if(not len(words) == 0):    
         words += suffixes[index]
-        
     return words;
     
 def getWords(number):
@@ -65,14 +44,41 @@ def getWords(number):
         count -= 1;
     final_words = ''
     for s in reversed(words):
-        temp = s + ' '
-        final_words += temp
+        final_words += s
     return final_words
-# End Main Logic
 
-# Loop and > to a file
-count = 0
-while count!=100:
-    print(getWords(count))
-    count=count+1;
+def main():
+    if upper<lower:
+        print("***Error: The max value is less than the start value.")
+        exit()
+    count=lower
+    while count<=upper:
+        print(getWords(count))
+        count=count+1;
+    exit()
 
+if __name__ == '__main__':
+    print("_________________________________________________")
+    print("                          __                  ")
+    print("   ____  __  ______ ___  / /_  ___  __________")
+    print("  / __ \/ / / / __ `__ \/ __ \/ _ \/ ___/ ___/")
+    print(" / / / / /_/ / / / / / / /_/ /  __/ /  (__  ) ")
+    print("/_/ /_/\__,_/_/ /_/ /_/_.___/\___/_/  /____/  ")
+    print("                                              ")
+    print("\r\nnumbers to string rep 0.2")
+    print("_________________________________________________\n\n")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-u", "--upper", help="number to end with")
+    parser.add_argument("-l", "--lower", help="number to start with")
+    args = parser.parse_args()
+    if args.upper and args.lower:
+        if args.upper.isnumeric() and args.lower.isnumeric():
+            upper = int(args.upper)
+            lower = int(args.lower)
+        else:
+            print("***Error: One of the values is not numeric")
+            exit()
+    else:
+        parser.print_help()
+        exit()
+    main()
